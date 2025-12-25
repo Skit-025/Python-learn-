d2dde512d42af27d5a85766682106d2e4e923c4f
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-25 21:06:51 +0530

    Create 04_problem.py

---
+++ Codes/CHAPTER 9/CHAPTER 9_PS/04_problem.py
@@ -0,0 +1,7 @@
+def word_replacer(word):
+    with open("donkey.txt","r") as f:
+        content=f.read()
+    new_content=content.replace(word,"######")
+    with open("donkey.txt","w") as f:
+        f.write(new_content)
+word_replacer("donkey")
