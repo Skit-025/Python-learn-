1bc265bcca71be700422902958bceda986f4ab3e
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-25 20:47:09 +0530

    Update 01_problem.py

--- Codes/CHAPTER 8/chaper 08_ps/01_problem.py
+++ Codes/CHAPTER 8/chaper 08_ps/01_problem.py
@@ -1,8 +1,16 @@
-def grt(a,b,c):
-    if a>b>c:
-        return a
-    elif(b>c>a):
-        return b
-    elif(c>b>a):
-        return c
-print("The greatest number :",grt(a=int(input("Enter number")),b=int(input("Enter number")),c=int(input("Enter number"))))
+# def grt(a,b,c):
+#     if a>b>c:
+#         return a
+#     elif(b>c>a):
+#         return b
+#     elif(c>b>a):
+#         return c
+# print("The greatest number :",grt(a=int(input("Enter number")),b=int(input("Enter number")),c=int(input("Enter number"))))
+num=int(input("Enter how many numnbers you want to  compare: "))
+def greatest():
+    l=[]
+    for i in range(num):
+        a=int(input("Enter number: "))
+        l.append(a)
+    return max(l)
+print("The greatest number is :",greatest())
