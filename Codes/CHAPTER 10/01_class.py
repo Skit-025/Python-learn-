a5e81a971ab97a14eef48cf47751a2b6b59653a8
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-27 22:27:55 +0530

    Create 01_class.py

---
+++ Codes/CHAPTER 10/01_class.py
@@ -0,0 +1,9 @@
+class Animal:
+    animal="dog"# this is a class attribute
+    animal1="cat" # this is a class attribute
+    sound="bark" # this is a class attribute
+    sound1="meow" # this is a class attribute
+    type="pet" # this is a class attribute
+
+obj=Animal()
+print(obj.animal)
