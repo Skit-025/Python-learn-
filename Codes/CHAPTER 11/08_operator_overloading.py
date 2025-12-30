cd4e7f1c857b9bd7b95aae44acd88bb91be639b3
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-30 19:17:52 +0530

    Create 08_operator_overloading.py

---
+++ Codes/CHAPTER 11/08_operator_overloading.py
@@ -0,0 +1,61 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    # String representation
+    def __str__(self):
+        return f"Number({self.value})"
+
+    # Addition
+    def __add__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+        return Number(self.value + other)
+
+    # Subtraction
+    def __sub__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value)
+        return Number(self.value - other)
+
+    # Multiplication
+    def __mul__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value)
+        return Number(self.value * other)
+
+    # Division
+    def __truediv__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value / other.value)
+        return Number(self.value / other)
+
+    # Equality
+    def __eq__(self, other):
+        if isinstance(other, Number):
+            return self.value == other.value
+        return self.value == other
+
+    # Less than
+    def __lt__(self, other):
+        if isinstance(other, Number):
+            return self.value < other.value
+        return self.value < other
+
+
+# --- Demo ---
+a = Number(10)
+b = Number(5)
+
+print("a =", a)              # Number(10)
+print("b =", b)              # Number(5)
+
+print("a + b =", a + b)      # Number(15)
+print("a - b =", a - b)      # Number(5)
+print("a * b =", a * b)      # Number(50)
+print("a / b =", a / b)      # Number(2.0)
+
+print("a == b:", a == b)     # False
+print("a < b:", a < b)       # False
+print("a == 10:", a == 10)   # True
+print("a + 20 =", a + 20)    # Number(30)
