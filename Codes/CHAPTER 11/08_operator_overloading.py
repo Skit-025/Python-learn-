e32d61df6128a862434c0dd98a9731731d28f37a
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-31 22:48:51 +0530

    Update 08_operator_overloading.py

--- Codes/CHAPTER 11/08_operator_overloading.py
+++ Codes/CHAPTER 11/08_operator_overloading.py
@@ -30,6 +30,12 @@ class Number:
             return Number(self.value / other.value)
         return Number(self.value / other)
 
+    # Floor division
+    def __floordiv__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value // other.value)
+        return Number(self.value // other)
+
     # Equality
     def __eq__(self, other):
         if isinstance(other, Number):
@@ -41,6 +47,10 @@ class Number:
         if isinstance(other, Number):
             return self.value < other.value
         return self.value < other
+
+    # Length (number of digits)
+    def __len__(self):
+        return len(str(abs(self.value)))
 
 
 # --- Demo ---
@@ -54,6 +64,10 @@ print("a + b =", a + b)      # Number(15)
 print("a - b =", a - b)      # Number(5)
 print("a * b =", a * b)      # Number(50)
 print("a / b =", a / b)      # Number(2.0)
+print("a // b =", a // b)    # Number(2)
+
+print("len(a) =", len(a))    # 2 (since "10" has 2 digits)
+print("len(Number(12345)) =", len(Number(12345)))  # 5
 
 print("a == b:", a == b)     # False
 print("a < b:", a < b)       # False
