718262e2b229f0e147804d509b5961a57a5b5e09
Aditya prasad Barik <codesheritage@gmail.com>
2025-12-31 22:49:09 +0530

    Create 06_problem.py

---
+++ Codes/CHAPTER 11/CHAPTER 11 PS/06_problem.py
@@ -0,0 +1,34 @@
+class vector:
+    def __init__(self,A,B,C):
+            self.A=A
+            self.B=B
+            self.C=C
+            
+    def __add__(self,vc1,):
+        X=self.A+vc1.A
+
+        Y=self.B+vc1.B
+
+        Z=self.C+vc1.C
+
+        return f"Vector{X,Y,Z}"
+    def __mul__(self,vc1,):
+        X=self.A*vc1.A
+
+        Y=self.B*vc1.B
+
+        Z=self.C*vc1.C
+
+        return f"{X+Y+Z}"
+    
+    def __str__(self):
+        return f"{self.A}i^+{self.B}j^+{self.C}k^"
+    
+a=vector(4,1,9)
+b=vector(10,2,3)
+c=vector(16,11,17)
+print(a+b)
+print(a*b)
+print(a)
+print(b)
+print(c)
