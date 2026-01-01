bc2414ff6ecd4a5502c769aa773de8ca4b645864
Aditya prasad Barik <codesheritage@gmail.com>
2026-01-01 19:30:34 +0530

    Create 03_problem.py

---
+++ Codes/CHAPTER 12/CHAPTER 12 PS/03_problem.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the number you want table of: "))
+L=[f"{n}x{i}={n*i}" for i in range(1,11)]
+print(L)
+
+# num=[1,2,3,4,5,6,7,8,9,10]
+# l1=[number for number in num if number<8]
+# print(l1)
